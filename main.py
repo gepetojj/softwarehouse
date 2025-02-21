c951f9859d1225dd792c35ae114f0a0dfa2f4658
João Pedro <nmjoaopedro22@gmail.com>
2025-02-20 21:50:45 -0300

    feat: implementa clear e select no Terminal

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
 from src.lib.terminal import Terminal
 
-Terminal.print_header("Hello, World!")
+
+while True:
+    Terminal.clear()
+    Terminal.print_header("Bem vindo ao sistema de gerenciamento")
+    print("\n")
+
+    try:
+        option = Terminal.select(
+            "O que deseja fazer?",
+            ["Entrar", "Cadastrar", "Sair"]
+        )
+    except ValueError as e:
+        Terminal.clear()
+        print(e)
+        input("Pressione Enter para continuar...")
+        continue
+
+    match option:
+        case 0:
+            continue
+        case 1:
+            continue
+        case 2:
+            Terminal.clear()
+            Terminal.print_header("Até logo!")
+            break
+        case _:
+            Terminal.clear()
+            print("Opção inválida.")
+            input("Pressione Enter para continuar...")
--- src/lib/terminal.py
+++ src/lib/terminal.py
@@ -3,9 +3,29 @@ class Terminal:
         pass
 
     @staticmethod
+    def clear():
+        print("\033[H\033[J")
+
+    @staticmethod
     def print_header(title: str, padding_x: int = 2, padding_y: int = 1):
         print(f"\n{'=' * (len(title) + padding_x * 2)}")
         print("\n" * (max(0, padding_y - 1)))
         print(f"{' ' * padding_x}{title}")
         print("\n" * (max(0, padding_y - 1)))
         print(f"{'=' * (len(title) + padding_x * 2)}")
+
+    @staticmethod
+    def select(label: str, options: list[str]) -> int:
+        print(label + "\n")
+        for i, option in enumerate(options):
+            print(f"{i + 1}. {option}")
+        print()
+
+        try:
+            value = int(input("Escolha uma opção: ")) - 1
+            if value < 0 or value > len(options) - 1:
+                raise ValueError("Opção inválida.")
+
+            return value
+        except:
+            raise ValueError("Opção inválida.")
